f0c4d09051a145e5e625049e22b1691d913227dd
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2020-01-28 13:57:07 -0500

    Update b300116593.py
    mon programme

--- 2.SelectionSort/b300116593.py
+++ 2.SelectionSort/b300116593.py
@@ -1,7 +1,9 @@
+#Algoritheme tri par selection
+
 def findSmallest(arr):
         smallest = arr[0]
         smallest_index = 0
-        for i in range(2, len(arr)):
+        for i in range(1, len(arr)):
             if arr[i] < smallest:
                 smallest = arr[i]
                 smallest_index = i
