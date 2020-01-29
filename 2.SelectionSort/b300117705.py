4e34060e2e9e56a0686d3986d3886c3d2e542c60
playboikz <acorligs@gmail.com>
2020-01-29 14:13:16 -0500

    :like:

--- 2.SelectionSort/b300117705.py
+++ 2.SelectionSort/b300117705.py
@@ -5,23 +5,24 @@ Created on Tue Jan 28 13:54:52 2020
 @author: acorl
 """
 
-def sort(nums):
+def findSmallest(arr):
+    
+        smallest = arr[0]
+        smallest_index = 0
+        
+        
+        for i in range(1, len(arr)):
+            if arr[i] < smallest:
+                smallest = arr[i]
+                smallest_index = i
+        return smallest_index
 
-    for i in range(5):
-        minpos = i
-        for j in range(i,6):
-            if nums[j] < nums[minpos]:
-                minpos = j
-
-
-        temp = nums[i]
-        nums[i] = nums[minpos]
-        nums[minpos] = temp
-
-        print(nums)
-
-
-nums = [5, 3, 8, 6, 7, 2]
-sort(nums)
-
-print(nums)  
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+     
+      smallest = findSmallest(arr)
+      
+      newArr.append(arr.pop(smallest))
+  return newArr
+print (selectionSort([5, 3, 6, 22, 55]))
