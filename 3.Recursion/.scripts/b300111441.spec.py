d7a14a29d07982b6dc012ad3b0b1c3c71d0e8ba5
Setrar <setrar@valiha.com>
2020-02-04 15:20:15 -0500

    .

---
+++ 3.Recursion/.scripts/b300111441.spec.py
@@ -0,0 +1,9 @@
+import sys; sys.path.append('.') # Rajouter le repertoire courant
+from b300111441 import fact
+test = fact( 5 ) == 120 
+if  test:
+     print('--------------------')
+     print(':tada: :tada: :tada:')
+else:
+     print('--------------------')
+     print(':no_entry: :no_entry: :interrobang:')
