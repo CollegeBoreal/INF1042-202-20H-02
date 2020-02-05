674b85dff96f641e49a88e7b544adefa404565c3
playboikz <acorligs@gmail.com>
2020-02-05 14:32:29 -0500

    :baby:

--- F.FunctionalProgramming/b300117705.py
+++ F.FunctionalProgramming/b300117705.py
@@ -16,3 +16,23 @@ j = [x for x in range(10) if x % 2 == 0]
 print(j)
 
 g =  [x for x in range(10) if x % 2 != 0 ]
+
+# set
+a = {2,2,2,2}
+
+print(a)
+
+a = {3, 3, 2, 2, 2}
+print (a)
+
+b = {'a' : 3, 'b': 4, 'c' : 5}
+print (b)
+
+a, b, c = (8, 6 , 10)
+print (a,b,c)
+
+
+x, y, z = (1, 1/2, 0.2)
+print (x, y, z)
+
+a, b, c = (8, 6, 10)
