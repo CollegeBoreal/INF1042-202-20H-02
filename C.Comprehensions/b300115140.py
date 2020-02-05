97cf814686c8067bd13bf69c6a76721bf587b4fc
ZackTO <zackstoronto@gmail.com>
2020-02-05 13:48:50 -0500

    Zack B.G.2

--- C.Comprehensions/b300115140.py
+++ C.Comprehensions/b300115140.py
@@ -8,8 +8,8 @@ Exercice sur ccode comprehension
 def rightTriangle(max):
     l = [(a, b, c)
     for c in range(1, 11)
-    for a in range(1, 11)
-    for b in range(1, 11)
+    for a in range(1, c + 1)
+    for b in range(1, a + 1)
     if a**2 + b**2 == c**2 and a + b + c == 24]
  
     return l
