e6c37d5fd2853d7999bdc22e49a18a71ac3da6ea
morti747 <54910700+morti747@users.noreply.github.com>
2020-02-11 11:55:40 -0500

    Morti

---
+++ C.Comprehensions/b300117314.py
@@ -0,0 +1,24 @@
+
+
+"""
+Created on Wed Feb 10 15:04:43 2020
+@author: 10
+"""
+
+def rightTriangle (max):
+
+    rightTriangle = [(a, b, c)
+#Expression
+    for c in range (1, 11)
+    for a in range (1, c)
+    for b in range (1, a)
+
+     if a**2 + b**2 == c**2 and  a + b + c == 24]
+#valeur
+
+    return rightTriangle
+print (rightTriangle(24))
+
+
+
+
