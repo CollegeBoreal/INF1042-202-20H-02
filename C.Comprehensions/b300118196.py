e300f4014dd4e4557460fb906a0d4e98c4226fc4
franck1999-30 <christinnguekam@gmail.com>
2020-02-11 13:13:18 -0500

    fine !

---
+++ C.Comprehensions/b300118196.py
@@ -0,0 +1,16 @@
+# -*- coding : utf-8 -*-
+
+import math
+import random
+
+def rightTriangle(max):
+    x = [ (a, b, c)
+    for c in range (11)
+    for a in range (1,c+1)
+    for b in range (1,a+1)
+    
+
+    if a**2 + b**2 == c**2 and a + b + c == 24]
+    print(x)
+
+rightTriangle(24)
