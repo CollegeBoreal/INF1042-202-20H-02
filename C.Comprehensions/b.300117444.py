438cca02c066d06520685fd9418650c3c7310f60
dido05327 <54910261+dido05327@users.noreply.github.com>
2020-02-11 12:23:43 -0500

    vive JSK

---
+++ C.Comprehensions/b.300117444.py
@@ -0,0 +1,13 @@
+def rightTriangle(max):
+
+    rightTriangle = [(a, b, c)
+#Les Expression
+    for c in range (1, 11)
+    for a in range (1, c)
+    for b in range (1, a)
+
+     if a**2 + b**2 == c**2 and  a + b + c == 24]
+#Les valeures
+
+    return rightTriangle
+print (rightTriangle(24))
