a99de407104e04fda99d4b222a0f7dfb49293286
playboikz <acorlings@gmail.com>
2020-02-11 15:37:12 -0500

    My message

---
+++ C.Comprehensions/b300117705.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def rightangle(max):
+
+    l = [(a, b, c)
+
+    for c in range(1, 11)
+
+    for a in range(1, c + 1)
+
+    for b in range(1, a + 1)
+
+    if a**2 + b**2 == c**2 and a + b + c == 24]
+
+ 
+
+    return l
+
+print (rightangle(24))
+
