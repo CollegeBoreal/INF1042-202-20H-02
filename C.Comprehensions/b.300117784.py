f04f381af231876172793a6aa1c8b79427b98016
BertrandMoyou <moyoubertrand@gmail.com>
2020-02-12 13:21:15 -0500

    :Fish;

--- C.Comprehensions/b.300117784.py
+++ C.Comprehensions/b.300117784.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 12 13:16:19 2020
+Created on Wed Feb 12 13:18:43 2020
 
 @author: Bertrand
+
 """
-
-#Triangle Rectangle 
-
 
 def rightTriangle(max):
    x=[(a,b,c) for a in range(11) for b in range(11)  for c in range (11) if (a**2+b**2==c**2) and (a+b+c==24)]
