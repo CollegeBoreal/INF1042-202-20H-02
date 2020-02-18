0b3d506926652c99b848c25b2bc841390a96acb1
dido05327 <54910261+dido05327@users.noreply.github.com>
2020-02-18 15:10:55 -0500

    ⵣ

--- 4.QuickSort/b300117444.py
+++ 4.QuickSort/b300117444.py
@@ -1,5 +1,10 @@
+"""
+Created on Tue Feb 11 15:28:06 2020
 
-** Athor : Idir **
+@author: iDIR
+"""
+
+
 def quicksort(array):
     if len(array) < 2:
         return array
@@ -8,4 +13,4 @@ def quicksort(array):
         less = [i for i in array[1:] if i<= pivot] 
         greater = [i for i in array[1:] if i > pivot]
         return quicksort(less) + [pivot] + quicksort(greater)
-print(quicksort([22, 4, 84, 400, 350, 500, 11]))  
+print(quicksort([22, 4, 84, 400, 350, 499,500, 11])) 
