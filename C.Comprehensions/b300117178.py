67d7b619c42448806737370b1c96a2d9bcacd7d9
willfrid boris <boriskwayep35@yahoo.com>
2020-02-18 14:59:29 -0500

    :whale

--- C.Comprehensions/b300117178.py
+++ C.Comprehensions/b300117178.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  5 11:15:33 2020
+Created on Tue Feb 18 14:11:55 2020
 
 @author: willfrid boris
 """
-
 def rightTriangle(max):
-   x=[(a,b,c) for a in range(11) for b in range(11)  for c in range (11) if (a**2+b**2==c**2) and (a+b+c==24)]
+
+   x=[(a,b,c) for c in range(11) for a in range(1,c)  for b in range (1,a) if (a**2+b**2==c**2) and (a+b+c==24)]
+
    return (x)
+
 print(rightTriangle(24))
