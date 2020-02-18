6760f7262794c4f8be540bbddb8fa323c4bcfc31
ZackTO <zackstoronto@gmail.com>
2020-02-18 15:07:29 -0500

    dont touch it anymore!

--- C.Comprehensions/b300115140.py
+++ C.Comprehensions/b300115140.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 12 13:16:19 2020
 
-@author: Bertrand
-"""
-
-#Triangle Rectangle 
-
+# exercice comprehension
 
 def rightTriangle(max):
-   x=[(a,b,c) for a in range(11) for b in range(11)  for c in range (11) if (a**2+b**2==c**2) and (a+b+c==24)]
-   return (x)
-print(rightTriangle(24))
+    l = [(a, b, c)
+    for c in range(1, 11)
+
+    for a in range(1, c + 1)
+    for b in range(1, a + 1)
+    if a**2 + b**2 == c**2 and a + b + c == 24]
+
+    return l
+
+print (rightTriangle(24))
