7f1ce44e259871f504d531538227b94e5f748f3e
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-02-18 14:56:19 -0500

    amir

--- 1.BinarySearch/b300116685.py
+++ 1.BinarySearch/b300116685.py
@@ -6,10 +6,27 @@ Created on Tue Feb 11 14:26:59 2020
 """
 
 
-def binary_search(num_list) :
-    if len (num_list) ==1 :
-        return num_list [0]
-    else:
-        return  num_list [0]+ binary_search(num_list[1:])
+def binary_search(list,article):
+    ValeurMini = 0
+    ValeurMaxi = len(ma_liste)-1
+    
+    
+    while ValeurMini <= ValeurMaxi:
+        mid= (ValeurMini + ValeurMaxi)//2
+        guess = list[mid]
+        if guess == article :
+           return mid
+        if guess > article:
+           ValeurMaxi = mid - 1
+        else:
+            ValeurMini = mid + 1
+    return None 
 
-print(binary_search([2,4,5,6,7])) 
+
+
+ma_liste = [1,7,11,14,19,20,25]
+
+
+print (binary_search(ma_liste, 7))
+print (binary_search(ma_liste, 20))
+print (binary_search(ma_liste, 21))
