038e6adc07ae8082bf57c6b9acc447231c63418a
amirali175 <54910751+amirali175@users.noreply.github.com>
2020-02-23 19:19:32 -0500

    Update b300116685.py

--- 2.SelectionSort/b300116685.py
+++ 2.SelectionSort/b300116685.py
@@ -2,23 +2,27 @@ def selectionSort(arr):
     smallest = arr[0]
     smallest_index = 0
 
-    for i in range(1, len(arr)):
+for i in range(1, len(arr)):
 
-        if arr[i] < smallest:
+
+    if arr[i] < smallest:
             smallest = arr[i]
             smallest_index = i
+            
+ 
+      return smallest_index
 
-    return smallest_index
 
 
 def selectionSort(arr):
     nouveau_arr = []
 
-    for i in range(len(arr)):
+for i in range(len(arr)):
         smallest = selectionSort(arr)
         nouveau_arr.append(arr.pop(smallest))
 
-    return nouveau_arr
+  
+     return nouveau_arr
 
 
-print(selectionSort([16, 2, 7, 4, 5, 33]))
+print(selectionSort([5,3,6,2,10]))
