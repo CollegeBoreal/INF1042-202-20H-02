5cd0f6e059c79342ea10512f4795ea18f27a16be
amirali175 <amirdek511@gmail.com>
2020-03-01 18:59:25 -0500

    :horse:

--- 1.BinarySearch/b300116685.py
+++ 1.BinarySearch/b300116685.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 18:13:34 2020
+Created on Sun Mar  1 18:57:44 2020
 
 @author: Amir
 """
+
 def binary_search (list, item) :
     low = 0
     high = len (list) -1
@@ -23,26 +24,6 @@ def binary_search (list, item) :
             low = mid + 1
             
 
-<<<<<<< HEAD
-def binary_search (list, item) :
-    low = 0
-    high = len (list) -1
-   
-    while low <= high :
-        
-        mid = (low + high)//2
-        guess = list[mid]
-        
-        if guess == item :
-            return mid
-        
-        if guess > item :
-            high = mid -1
-        
-        else :
-            low = mid + 1
-            
-=======
     return None
 
 my_list = [5, 7, 7, 11, 14, 19, 20, 25]
@@ -51,16 +32,3 @@ my_list = [5, 7, 7, 11, 14, 19, 20, 25]
 
 print (binary_search (my_list, 25))
 print (binary_search (my_list, 14))
->>>>>>> e1c2bbe03e8b7a3161aecaf721eb449ac36411fc
-
-    return None
-
-my_list = [5, 7, 7, 11, 14, 19, 20, 25]
-
-
-
-<<<<<<< HEAD
-print (binary_search (my_list, 25))
-print (binary_search (my_list, 14))
-=======
->>>>>>> e1c2bbe03e8b7a3161aecaf721eb449ac36411fc
