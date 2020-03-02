901b924edbc6b26892d724be018cee4032049376
amirali175 <amirdek511@gmail.com>
2020-03-01 19:03:34 -0500

    :horse:

--- 2.SelectionSort/b300116685.py
+++ 2.SelectionSort/b300116685.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 18:03:52 2020
+Created on Sun Mar  1 18:58:20 2020
 
 @author: Amir
 """
-=======
->>>>>>> e1c2bbe03e8b7a3161aecaf721eb449ac36411fc
+
 def trouvepetit (arr):
     petit = arr [0]
     petit_index = 0
@@ -23,4 +21,4 @@ def selectionSort (arr):
         newArr.append(arr.pop(petit))
     return newArr
 
-print (selectionSort([10, 90, 80, 20, 30]))
+print (selectionSort([5, 1, 8, 20, 3]))
