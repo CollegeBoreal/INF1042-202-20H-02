90f72d0726dd4cd8ab8030bccfda75902e4a5881
BertrandMoyou <moyoubertrand@gmail.com>
2020-03-04 15:44:08 -0500

    :fish:

--- 6.HashTables/b300117784.py
+++ 6.HashTables/b300117784.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 15:13:09 2020
 
+@author: Bertrand
+"""
+
+Musiques = {}
+
+Musiques['Africaine'] = ['Makossa', 'Ndombolo','Coupedecale','Bensikin']
+
+Musiques['Europenne'] = ['Jazz','classique','Versaille','Salsa']
+
+print(Musiques)
+
+def ajout(tuple):
+    k,v = tuple
+       
+    Musiques [k] = v
+       # rajouter le tuple
+       
+    return Musiques
+
+print (ajout(('ma_cle',[0,1,2,3,4])))
