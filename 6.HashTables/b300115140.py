ab1f746905bee590966a94bbcdc4a633bacee289
Zack <54910329+ZackTO@users.noreply.github.com>
2020-03-04 15:28:29 -0500

    Update b300115140.py

--- 6.HashTables/b300115140.py
+++ 6.HashTables/b300115140.py
@@ -24,3 +24,8 @@ recettes['coucous'] = ['semoule', 'tomates', 'poivrons', 'boeuf', 'pois-chiches'
 def ajout(tuple):
    # rajouter le tuple 
    return recettes
+
+[ x for x in recettes.keys() ]
+[ x for x in recettes.values() ]
+
+
