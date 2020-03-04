a1798f04e6921eef4564b46f891cc92ec48c7254
dido05327 <54910261+dido05327@users.noreply.github.com>
2020-03-04 13:52:33 -0500

    ⵣⵣⵣⵣⵣⵣ

--- 5.FP/b300117444.py
+++ 5.FP/b300117444.py
@@ -5,12 +5,23 @@ Created on Tue Feb 11 15:28:06 2020
 @author: iDIR
 """
 
+# Ecrire Un programme qui utilise map avec une fonction anonyme
+#avec un generateur allant jusqyu'a 10
+
+
+w=map(lambda x: x * 2,range(10))
+
+print(list(w))
+
+# Filter
+
+filter(lambda x: x >= 5, range(20))
+
+list(filter(lambda x: x >= 5, range(20)))
+
+
+#Reduction
+
 import functools
-t=map(lambda x:x*2,range(10))
 
-print(list(t))
-
-print(list(filter(lambda x:x>=5,range(20))))
-
-print(functools.reduce(lambda x, y :x+y,range(10)))
-
+functools.reduce(lambda x,y: x + y,range(10))
