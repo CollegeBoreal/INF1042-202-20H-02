f608f6a3450bdb22c875874371ce06272480dea1
morti747 <54910700+morti747@users.noreply.github.com>
2020-03-04 14:53:27 -0500

    Morti

--- 6.HashTables/b300117314.py
+++ 6.HashTables/b300117314.py
@@ -1,1 +1,21 @@
 
+"""
+Created on Wed Mar  4 14:24:30 2020
+
+@author: Morti
+"""
+
+
+
+robert={}
+robert ['a'] = 1
+print (robert)
+
+
+étudiants = {}
+étudiants[300117314]= 'Morti'
+étudiants[300117444]= 'Idir'
+print(étudiants)
+
+
+
