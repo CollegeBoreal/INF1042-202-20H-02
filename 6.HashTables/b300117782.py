cdc9bf1b5446b0da07505576d1a4331893b158be
Raissa111 <ernanjia@gmail.com>
2020-03-04 15:12:33 -0500

    bonjour

--- 6.HashTables/b300117782.py
+++ 6.HashTables/b300117782.py
@@ -1,5 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 14:19:52 2020
 
+@author: User
+"""
 erna = {}
 
 erna[ 'a' ] = 1
 print (erna)
+
+erna['b']=2
+print ( erna )
+
+etudiant = {}
+etudiant[300116670] = 'auriane'
+etudiant[300117782] = 'raissa'
+print( etudiant)
+
+book =dict()
+book['pomme'] = .99
+book['avocat'] = 2.5
+book['carotte'] = 3 
+print(book)
