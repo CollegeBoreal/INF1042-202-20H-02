a62cd2304a39aab074ecf9b2165cf56c4f22055c
ZackTO <zackstoronto@gmail.com>
2020-03-04 14:53:28 -0500

    zack hash table

--- 6.HashTables/b300115140.py
+++ 6.HashTables/b300115140.py
@@ -1,1 +1,20 @@
-#Mon code HashTable
+#Mon code HashTable Zack
+
+robert = {}
+robert ['a'] = 1
+robert ['b'] = 2
+print (robert)
+
+print (robert ['b'])
+etudiants = {}
+etudiants [3001166782] = 'Aurianne'
+etudiants [3001176700] = 'Erna'
+
+print (etudiants)
+
+book = dict()
+book ['pomme'] = .99
+book ['avocat'] = 2.5
+book ['atieke'] = 5.0
+
+print (book)
