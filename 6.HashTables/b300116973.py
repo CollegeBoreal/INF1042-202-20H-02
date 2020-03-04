7cda0cac7eb518e3dab4e4a22815f7aaa09d136f
unknown <lagokamdem87@gmail.com>
2020-03-04 14:43:08 -0500

    :sad:

---
+++ 6.HashTables/b300116973.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 14:05:56 2020
+
+@author: dell
+"""
+
+robert = {}
+robert ["a"] = 1
+print (robert)
+
+robert ['b'] = 2
+
+print (robert)
+etudiants = {}
+etudiants [300116673] = "nathalie"
+etudiants [300116670] = "auriane"
+etudiants [300117782] = "erna"
+print (etudiants)
