1b4ce8a9f837b97c8b1d8623e7386fb8d9ee3ff9
willfrid boris <boriskwayep35@yahoo.com>
2020-03-04 14:10:20 -0500

    :whale:

---
+++ 6.HashTables/b300117178.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 14:06:22 2020
+
+@author: willfrid boris
+"""
+
+matiere={}
+matiere["mathematique"]="brice robert"
+matiere["reseau"]="richard"
+print(matiere)
