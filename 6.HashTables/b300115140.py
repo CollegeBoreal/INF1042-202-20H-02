0bacf12bd7db1da1915fa1ee31469244ac8002fe
Zack <54910329+ZackTO@users.noreply.github.com>
2020-03-08 15:20:13 -0400

    Update b300115140.py

--- 6.HashTables/b300115140.py
+++ 6.HashTables/b300115140.py
@@ -1,31 +1,14 @@
 #Mon code HashTable
-robert = {}
-robert ['a'] = 1
-robert ['b'] = 2
-print (robert)
-print (robert ['b'])
-etudiants = {}
-etudiants [3001166782] = 'Aurianne'
-etudiants [3001176700] = 'Erna'
 
-print (etudiants)
 
-book = dict()
-book ['pomme'] = .99
-book ['avocat'] = 2.5
-book ['atieke'] = 5.0
-
-print (book)
-
-recettes = {}
-recettes['mafe'] = ['huile', 'eau', 'maggi', 'poisson', 'epices', 'tomates']
-recettes['coucous'] = ['semoule', 'tomates', 'poivrons', 'boeuf', 'pois-chiches']
+recetteskabyles = {}
+recetteskabyles ['hasbana'] = ['huile', 'tomtes', 'semoule', 'boeuf', 'razelhanout', 'menthe']
+recetteskabyles ['brickaloeuf'] = ['feuilledebrick', 'oeufs', 'fromage', 'epices', 'huile']
 
 def ajout(tuple):
-   # rajouter le tuple 
-   return recettes
+   return recetteskabyles
 
-[ x for x in recettes.keys() ]
-[ x for x in recettes.values() ]
+[ x for x in recetteskabyles.keys() ]
+[ x for x in recetteskabyles.values() ]
 
-
+print (ajout(('ma_cle',[0,1,2,3,4])))
