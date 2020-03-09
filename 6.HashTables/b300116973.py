43bf8b42f9d47ac286c039c1afb640af30b89037
unknown <lagokamdem87@gmail.com>
2020-03-09 15:39:04 -0400

    :see_no_evil:

--- 6.HashTables/b300116973.py
+++ 6.HashTables/b300116973.py
@@ -5,37 +5,26 @@ Created on Wed Mar  4 14:05:56 2020
 @author: dell
 """
 
-robert = {}
-robert ["a"] = 1
-print (robert)
+livre = {}
 
-robert ['b'] = 2
+livre['classique'] = ['moumbutu', 'jean de dieu momo','mamadou et binera','mon ecole']
 
-print (robert)
-etudiants = {}
-etudiants [300116673] = "nathalie"
-etudiants [300116670] = "auriane"
-etudiants [300117782] = "erna"
-print (etudiants)
-t = ('a', 5.0)
-a, b = t
-print (t)
+livre['fiction'] = ['commando','brussle','24heurechrono','matrice']
+
+print(livre)
+
+def ajout(tuple):
+    l,m = tuple
+       
+    livre [l] = m
+       # rajouter le tuple
+       
+    return livre
+
+print (ajout(('ma_cle',[0,1,2,3,4])))
 
 
-recette = {}
-recette ["mafe"] = ['huile', 'eau', 'poisson', 'epices', 'tomates']
-recette ['canadienne']=['maple', 'spaguetti', 'champignons', 'pizza']
 
-print(recette)
-
-def ajout (tuple):
-    x,y =tuple
-    
-    recette [x] = y
-    
-    return recette
-
-print (ajout(('ma_cle', [0,1,2,3,4])))
 
 
 
