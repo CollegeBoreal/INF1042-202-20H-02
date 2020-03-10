5fc1a5913869db81fdb8d75f882292bc342ea42c
zoureni <zoureni@outlook.fr>
2020-03-10 15:21:55 -0400

    zoureni

---
+++ 7.BFS/b300118524.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 15:08:46 2020
+
+@author: zoureni
+"""
+
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana"]
+eleves["Erna"] = []
+eleves["Hassana"] = []
+eleves["Zouréni"] = ["Sekou", "Auriane", "Corlings"]
+eleves["Sekou"] = []
+eleves["Auriane"] = []
+eleves["Corlings"] = []
+eleves["Aldelkrim"] = ["Souleymane", "Zack"]
+eleves["Souleymane"] = []
+eleves["Zack"] = []
