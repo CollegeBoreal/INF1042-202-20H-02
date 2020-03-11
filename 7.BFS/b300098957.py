feeec710d4a23122d3e503809eee374b450b52e9
Setrar <setrar@valiha.com>
2020-03-11 14:56:12 -0400

    .

--- 7.BFS/b300098957.py
+++ 7.BFS/b300098957.py
@@ -1,17 +1,38 @@
 # BFS
 
 eleves = {}
-eleves["Boris"]=["Amir","Franck","Nathalie"]
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
 eleves["Amir"]=[]
 eleves["Franck"]=[]
 eleves["Nathalie"]=[]
-eleves["Bertrand"]=["Erna","Hassana"]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
 eleves["Erna"]=[]
 eleves["Hassana"]=[]
 eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
 eleves["Sekou"]=[]
 eleves["Auriane"]=[]
 eleves["Corlings"]=[]
-eleves["Abdelkrim"]=["Souleyman","Zack"]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
+
+def personne_elue(name):
+    return name == 'Zoureni'
+
+from collections import deque
+
+def search(name):
+   search_queue = deque()
+   search_queue += eleves[name]
+   while search_queue:
+      personne = search_queue.popleft()
+      if personne_elue(personne):
+         print(personne + " a le fameux Mac")
+         return True
+      else:
+         search_queue += eleves[personne]
+   return False
+
+search("Boris")
+
+
