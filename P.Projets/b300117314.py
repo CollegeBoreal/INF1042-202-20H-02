a55bb96a6ee5ce921685c8548f0e19e884cd9690
morti747 <54910700+morti747@users.noreply.github.com>
2020-03-16 13:49:19 -0400

    Morti

--- P.Projets/b300117314.py
+++ P.Projets/b300117314.py
@@ -3,13 +3,18 @@ Created on Mon Mar 16 13:09:48 2020
 
 @author: Morti
 """
+"""
+Created on Mon Mar 16 13:09:48 2020
+
+@author: Morti
+"""
 
 import getpass
 import sys
 import telnetlib
 
-HOST = "192.168.122.71"
-user = input("Enter your telnet username: ")
+HOST = "192.168.163.154"
+user = input("Enter your username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
@@ -23,9 +28,9 @@ if password:
 tn.write("enable\n")
 tn.write("cisco\n")
 tn.write("conf t\n")
-tn.write("int loop 0\n")
+tn.write("int loop 3\n")
 tn.write("ip address 1.1.1.1 255.255.255.255\n")
-tn.write("int loop 1\n")
+tn.write("int loop 4\n")
 tn.write("ip address 2.2.2.2 255.255.255.255\n")
 tn.write("router ospf 1\n")
 tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
