007394e8a389a39a7719f16964e37b1c94ac5e50
morti747 <54910700+morti747@users.noreply.github.com>
2020-03-22 20:44:18 -0400

    Morti

--- 5.FP/b300117314.py
+++ 5.FP/b300117314.py
@@ -4,14 +4,14 @@
 """
 
 
-a = list ( map( lambda x: x * 2 , range(10)))
-print(a)
+import functools
 
+x = map (lambda x : x*2, range(15))
+a = list (x)
+print (a)
 
-b = list ( filter( lambda x: x >= 5 , range(20)))
+b = list(filter (lambda x : x > 5 , range(15)))
 print(b)
 
-
-import functools
-g = functools.reduce( lambda x, y : x +y , range(10))
-print(g)
+c = functools.reduce(lambda x,y : x+y , range (15))
+print (c)
