1eb8d6f6b9ad83d651f27ab808d723df1b811fe2
BertrandMoyou <54910102+BertrandMoyou@users.noreply.github.com>
2020-03-31 17:31:16 -0300

    Create b300117784.py
    🐕

---
+++ 7.BFS/b300117784.py
@@ -0,0 +1,30 @@
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+
+def personne_elue (name) :
+  return name == 'Zoureni'
+
+def search (name) :
+  from collections import deque
+  visitees = []
+  search_queue = deque()
+  search_queue += eleves[name]
+  while search_queue:
+      personne = search_queue.popleft()
+      if not personne in visitees:
+        if personne_elue(personne):
+          print(personne + " a le fameux Mac")
+          return True
+      search_queue += eleves[personne]
+      visitees.append(personne)
+  return False
+
+if __name__== "__main__":
+ search("Boris")
