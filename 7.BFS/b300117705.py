ea88b354fcafb093591e3934f177b48e0aad57c1
playboikz <54909863+playboikz@users.noreply.github.com>
2020-04-01 14:01:34 -0400

    :fire:

--- 7.BFS/b300117705.py
+++ 7.BFS/b300117705.py
@@ -26,3 +26,26 @@ eleves["Corlings"]=[]
 eleves["Abdelkrim"]=["Souleyman","Zack"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
+
+
+def personne_elue(name):
+    return name == 'Zoureni'
+
+from collections import deque
+
+def search(name):
+   visitees = []
+   search_queue = deque()
+   search_queue += eleves[name]
+   while search_queue:
+      personne = search_queue.popleft()
+      if not personne in visitees:
+         if personne_elue(personne):
+            print(personne + " a le fameux Mac")
+            return True
+         search_queue += eleves[personne]
+         visitees.append(personne)
+   return False
+
+if __name__== "__main__":
+   search("Boris")
