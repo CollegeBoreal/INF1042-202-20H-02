8b32f2073b144bf7170db7dc930ec26e10bc89fe
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2020-04-01 13:35:05 -0400

    Update b300116973.py
    👍

--- 7.BFS/b300116973.py
+++ 7.BFS/b300116973.py
@@ -1,50 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 10 15:07:14 2020
 
-@author: dell
+@author: lagokamdem
 """
-
-#bfs
-
-eleves ={}
-eleves['boris'] = ['zourenie', 'franck','franck', 'amir']
-eleves['amir']=[]
-eleves["franck"]= []
-eleves["nathalie"]=[]
-eleves['bertrand']= ['erna', 'assana', 'boris']
-eleves ['erna']=[]
-eleves ["oriane"]=[]
-eleves["souleumane"]=[]
-eleves["zack"]=[]
-eleves["abdelkrim"]=['zourenie','sekou']
-eleves['assana']=[]
-eleves["erna"]=[]
-eleves["morteza"]=['idir','yassine']
-eleves['idir']=[]
-eleves["yassine"]=[]
-eleves["zoureni"]=['nathalie','fofana']
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
 
 def personne_elue(name):
-    return name == 'zoureni'
+    return name == 'Zoureni'
 
 from collections import deque
 
-def search(name) :
-    search_queue = deque ()
-    
+def search(name):
+    visitees = []
+    search_queue = deque()
     search_queue += eleves[name]
-    
-    visitee = []
-
     while search_queue:
-       personne=search_queue.popleft()
-       if not personne in visitee:
-          if personne_elue:
-             print(personne + " a le fameux mac")
+       personne = search_queue.popleft()
+       if not personne in visitees:
+          if personne_elue(personne):
+             print(personne + " a le fameux Mac")
              return True
-          else:
-             search_queue += eleves [personne]
-             visitee.append(personne)
+          search_queue += eleves[personne]
+          visitees.append(personne)
     return False
-search("boris")
+
+if __name__== "__main__":
+   search("Boris")
+
+
+
+
