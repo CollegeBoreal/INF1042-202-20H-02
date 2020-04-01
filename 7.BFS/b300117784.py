77b285d4ed0d0a982db5cbe52abcde8b135132c6
BertrandMoyou <54910102+BertrandMoyou@users.noreply.github.com>
2020-04-01 14:20:43 -0400

    :dog:

--- 7.BFS/b300117784.py
+++ 7.BFS/b300117784.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: BertrandMoyou
+"""
+
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
 eleves["Erna"]=[]
 eleves["Hassana"]=[]
 eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
