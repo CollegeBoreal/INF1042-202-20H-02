7a915016f0e0bae9bb8bb148cbff6f361e9744d3
Sekou16 <55207099+Sekou16@users.noreply.github.com>
2020-04-04 11:19:03 -0400

    :smile:

--- 7.BFS/b300111441-avant-contingence.py
+++ 7.BFS/b300111441-avant-contingence.py
@@ -1,22 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 10 15:09:03 2020
 
-@author: skofo
+@author: Sekou16
 """
 
-eleve = {}
-eleve ['boris']=['amir','franck','nathalie']
-eleve ['franck']=[]
-eleve ['amir']=[]
-eleve ['nathalie']=[]
-eleve ['bertrand']=['ernan','djibo']
-eleve ['ernan']=[]
-eleve ['djibo']=[]
-eleve ['zoureni']=['sekou','auriane','corlings']
-eleve ['sekou']=[]
-eleve ['auriane']=[]
-eleve ['corlings']=[]
-eleve ['krim']=['souleymane','zack']
-eleve ['souleymane']=[]
-eleve ['zack']=[]
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+
+def personne_elue(name):
+    return name == "Zoureni"
+
+from collections import deque
+
+def search(name):
+   visitees = []   
+     
+   search_queue = deque()
+   search_queue += eleves[name]
+   while search_queue:
+      personne = search_queue.popleft()
+      if not personne in visitees:
+         if personne_elue(personne):
+            print(personne + " a le fameux Mac")
+            return True
+         search_queue += eleves[personne]
+         visitees.append(personne) 
+   return false
+if __name__== "__main__":
+      search("Boris")
