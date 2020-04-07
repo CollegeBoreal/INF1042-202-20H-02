c5e9dcc694d539d9ca5cbd3fc10a3cfa4054900f
Sekou16 <55207099+Sekou16@users.noreply.github.com>
2020-04-07 19:36:09 -0400

    Update b300111441.py

--- 7.BFS/b300111441.py
+++ 7.BFS/b300111441.py
@@ -21,21 +21,22 @@ eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
 def personne_elue(name):
-    return name == "Zoureni"
-
-from collections import deque
+  return name == 'Zoureni'
 
 def search(name):
-   print( len(eleves.values()) )
-   return False   
-   search_queue = deque()
-   search_queue += eleves[name]
-   while search_queue:
-      personne = search_queue.popleft()
+  from collections import deque
+  visitees = []
+  search_queue = deque()
+  search_queue += eleves[name]
+  while search_queue:
+    personne = search_queue.popleft()
+    if not personne in visitees:
       if personne_elue(personne):
-         print(personne + " a le fameux Mac")
-         return True
-      search_queue += eleves[personne] 
-   return false
+        print(personne + " a le fameux Mac")
+        return True
+      search_queue += eleves[personne]
+      visitees.append(personne)
+  return False
+
 if __name__== "__main__":
-      search("Boris")
+  search("Boris")
