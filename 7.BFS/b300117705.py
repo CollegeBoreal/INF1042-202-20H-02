912653bbb9edccc11ac783ec98bd7d27bf9c76c3
playboikz <54909863+playboikz@users.noreply.github.com>
2020-04-08 14:16:57 -0400

    :blacky:

--- 7.BFS/b300117705.py
+++ 7.BFS/b300117705.py
@@ -6,27 +6,20 @@ Created on Tue Mar 10 15:23:03 2020
 """
 
 eleves = {}
-eleves ['Boris'] = ['Amir', 'Franck','Nathalie']
-eleves ['Amir'] = []
-eleves ['Franck'] = []
-eleves['Nathalie'] = []
-
-
-eleves['Bertrand'] = ['Erna','Hassana']
-eleves['Erna']=[]
-eleves['Hassna']=[]
-
-
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
 eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
 eleves["Sekou"]=[]
 eleves["Auriane"]=[]
 eleves["Corlings"]=[]
-
-
-eleves["Abdelkrim"]=["Souleyman","Zack"]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
-
 
 def personne_elue(name):
     return name == 'Zoureni'
@@ -34,18 +27,18 @@ def personne_elue(name):
 from collections import deque
 
 def search(name):
-   visitees = []
-   search_queue = deque()
-   search_queue += eleves[name]
-   while search_queue:
-      personne = search_queue.popleft()
-      if not personne in visitees:
-         if personne_elue(personne):
-            print(personne + " a le fameux Mac")
-            return True
-         search_queue += eleves[personne]
-         visitees.append(personne)
-   return False
+    visitees = []
+    search_queue = deque()
+    search_queue += eleves[name]
+    while search_queue:
+       personne = search_queue.popleft()
+       if not personne in visitees:
+          if personne_elue(personne):
+             print(personne + " a le fameux Mac")
+             return True
+          search_queue += eleves[personne]
+          visitees.append(personne)
+    return False
 
 if __name__== "__main__":
    search("Boris")
