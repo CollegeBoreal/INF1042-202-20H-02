a96a221e47d1291d3cfe290d3e82a071d02f6c2f
morti747 <54910700+morti747@users.noreply.github.com>
2020-04-10 18:55:55 -0400

    Update b300117314.py

--- P.Projets/300117314/b300117314.py
+++ P.Projets/300117314/b300117314.py
@@ -25,13 +25,11 @@ if password:
 tn.write("enable\n")
 tn.write("cisco\n")
 tn.write("conf t\n")
-tn.write("int loop 3\n")
+tn.write("int loop 1\n")
 tn.write("ip address 1.1.1.1 255.255.255.255\n")
-tn.write("int loop 4\n")
+tn.write("int loop 2\n")
 tn.write("ip address 2.2.2.2 255.255.255.255\n")
-tn.write("router ospf 1\n")
-tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
+
 tn.write("end\n")
 tn.write("exit\n")
-
 print (tn.read_all())
