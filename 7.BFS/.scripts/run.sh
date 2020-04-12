3369f7cde81aac9e705bff2fb65aa43415f19332
b300098957 <300098957@collegeboreal.ca>
2020-04-12 11:36:47 -0400

    .

--- 7.BFS/.scripts/run.sh
+++ 7.BFS/.scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sh .scripts/participation.sh > .scripts/Participation.md 2>&1
-sh .scripts/execution.sh > .scripts/Execution.md 2>&1
-sh .scripts/notation.sh >> .scripts/Participation.md 2>&1
+#sh .scripts/execution.sh > .scripts/Execution.md 2>&1
+#sh .scripts/notation.sh >> .scripts/Participation.md 2>&1
