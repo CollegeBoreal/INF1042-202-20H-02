a5d372271e4b25eb4a7760f20b40c91102d0d10b
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2020-04-14 11:20:43 -0700

    Delete C300116593.py

--- P.Projets/C300116593.py
+++
@@ -1,7 +0,0 @@
-#programmme python qui verfie un nombre entrer est paire impaire
-
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
